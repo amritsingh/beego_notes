916e9bda6a4c23f97e4437209022f2c92e09ed36
Amrit Pal Singh <amrit0403@gmail.com>
2023-09-23 14:44:57 +0530

    Old index page

--- controllers/default.go
+++ controllers/default.go
@@ -9,7 +9,7 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Title"] = "Notes App"
-	c.Data["LoggedIn"] = (c.GetSession("user_id") != nil)
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
 }
